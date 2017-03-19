35372e969d3f192418d70f19060a00a320b35096
Tuvix Shih <tuvix@beehivedt.com>
2017-03-19 19:49:30 +0800

    Get all link in page.

--- scrape1.py
+++ scrape1.py
@@ -4,3 +4,7 @@ from bs4 import BeautifulSoup
 html = urlopen("http://www.nccu.edu.tw")
 objbs = BeautifulSoup(html.read(), "html.parser")
 print(objbs.title)
+
+for link in objbs.findAll("a"):
+    if 'href' in link.attrs:
+        print(link.attrs["href"])
